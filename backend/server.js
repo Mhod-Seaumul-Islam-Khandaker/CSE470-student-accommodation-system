24f585f2cc84824ea9dab3b5d74cf59143d4cbb0
Seaum <seaumulislam@gmail.com>
2025-04-18 15:55:10 +0600

    Version 1.1: code organized and simplified

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectToDatabase = require('./config/db'); // Import the connection function
 
 // Load environment variables
 dotenv.config();
@@ -15,18 +16,12 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB using Mongoose
-mongoose.connect(process.env.MONGO_URI, {
-}).then(() => {
-  console.log('MongoDB connected!');
-}).catch((error) => {
-  console.log('MongoDB connection error:', error);
-});
+connectToDatabase();
 
 // Simple route
-app.get('/api/hello', (req, res) => {
+app.get('/test', (req, res) => {
   res.json({ message: 'Hello from backend' });
 });
-
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
